6a099f4c2edb2849d2a239eebd858bbf7b748539
roman <litsovr@gmail.com>
2026-07-02 14:42:58 +0000

    Add disk and memory health check script

---
+++ check_disk.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+THRESHOLD=20
+USAGE=$(df / | tail -1 | awk '{print $5}' | tr -d '%')
+
+echo "Текущее использование диска: $USAGE%"
+
+if [ "$USAGE" -gt "$THRESHOLD" ]; then
+    echo "ВНИМАНИЕ: диск заполнен более чем на $THRESHOLD%!"
+else
+    echo "Всё в порядке, места достаточно."
+fi
+
+
+MAXMEMORY=80
+MEMORY=$(free | grep Mem | awk '{printf "%.0f", $3/$2 * 100}')
+
+echo "Текущее использование памяти: $MEMORY%"
+
+if [ "$MEMORY" -gt "$MAXMEMORY" ]; then
+    echo "ВНИМАНИЕ: память заполнена более чем на $MAXMEMORY%!"
+else
+    echo "Всё в порядке, места достаточно."
+fi
+
+
+
+echo "Аптайм"
+uptime -p
